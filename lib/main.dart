d42c9902fa4c95ea70de6312c3ec24155816cc73
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-06-16 15:35:43 +0530

    ProviderScope
    Wrap the MaterialApp With ProviderScope
    
    install pachage : riverpod
                               flutter_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_2025/home%20screen.dart';
 
 void main() {
@@ -10,10 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      home: HomeScreen(),
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(),
+        home: HomeScreen(),
+      ),
     );
   }
 }
