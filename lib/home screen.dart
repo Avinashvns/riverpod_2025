95eae50288b49bc1b7e8f490b1f6fe377be42ea6
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-06-16 15:36:29 +0530

    create a provider variable

--- lib/home screen.dart
+++ lib/home screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// read a constant value and create a provider
+final data = Provider<String>((ref){
+  return "Avinash";
+});
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
