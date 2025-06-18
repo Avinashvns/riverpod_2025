44679ec2d6e69f7c849a3c804e8d8b6e40eceba7
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-06-19 00:19:20 +0530

    Future Provider with integer in Riverpod
    integer value

---
+++ lib/future_provider/future_provider.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final futureProvider = FutureProvider<int>((ref) async{
+  await Future.delayed(const Duration(seconds: 2));
+  // throw 'internet not working';
+  return 3;
+});
--- lib/home screen.dart
+++ lib/home screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_2025/future_provider/future_provider.dart';
 import 'package:riverpod_2025/item_provider.dart';
 import 'package:riverpod_2025/provider/favourite_provider.dart';
 import 'package:riverpod_2025/search_provider.dart';
@@ -19,54 +20,20 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final favouriteList = ref.watch(favouriteProvider);
+    final provider = ref.watch(futureProvider);
     print("build");
     return Scaffold(
-      appBar: AppBar(title: const Text("Counter App"),
-      actions: [
-        PopupMenuButton<String>(
-          onSelected: (value){
-            ref.read(favouriteProvider.notifier).favourite(value);
-          },
-            itemBuilder: (BuildContext context){
-            return const [
-              PopupMenuItem(child: Text("All"), value: 'All',),
-              PopupMenuItem(child: Text("Favourite"), value: 'Favourite',),
-            ];
-            }
-        )
-      ],),
+      appBar: AppBar(title: const Text("Future Provider"),
+     ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(favouriteProvider.notifier).addMyItem();
-        },
-        child: const Icon(Icons.add),
+
+      body: Center(
+        child: provider.when(
+          data: (value)=> Text(value.toString()),
+          error: (error,stack) => Text(error.toString()),
+          loading: ()=> const CircularProgressIndicator()
+        ),
       ),
-      body:
-          Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  border: OutlineInputBorder()
-                ),
-                onChanged: (value){
-                  ref.read(favouriteProvider.notifier).filterList(value);
-                },
-              ),
-              Expanded(child: ListView.builder(
-                itemCount: favouriteList.filterItems.length,
-                  itemBuilder: (context, index){
-                    final item = favouriteList.filterItems[index];
-                    return ListTile(
-                      title: Text(item.name),
-                      trailing: Icon(item.favourite ? Icons.favorite : Icons.favorite_border),
-                    );
-                  }
-              ))
-            ],
-          ),
     );
   }
 }
